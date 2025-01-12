4affe26815332a656aa60c734e80c4df1769e2bd
Mark <74938763+markrbu@users.noreply.github.com>
2025-01-12 14:57:05 +1100

    Only highlight grid cell if activeTool != move

--- src/components/Grid/GirdCell.jsx
+++ src/components/Grid/GirdCell.jsx
@@ -1,12 +1,27 @@
 // src/components/Grid/GridCell.jsx
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import * as THREE from 'three';
+import useMapStore from "../../store/mapStore.js";
 
 const GridCell = ({ size, sides = 4, position, rotation, cellColor = 'white'}) => {
+    const { activeTool } = useMapStore();
     const [hover, setHover] = useState(false);
+
+    const handleHover = (e) => {
+        e.stopPropagation();
+        if (activeTool === 'move') return;
+        setHover(true);
+    }
+
+    useEffect(() => {
+        if (activeTool === 'move') {
+            setHover(false);
+        };
+    }, [activeTool]);
+
     return (
         <mesh
-            onPointerOver={(e) => setHover(true)}
+            onPointerOver={handleHover}
             onPointerOut={(e) => setHover(false)}
             position={position}
             rotation={rotation}
